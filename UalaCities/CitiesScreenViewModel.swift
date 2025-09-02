2e09b73b84c21ecdcc0d10cb8fb00e91fb04b450
Leandro Linardos <leandro.linardos@gmail.com>
2025-09-01 21:59:29 -0300

    refactor duplicated code on fetch cities

--- UalaCities/CitiesScreenViewModel.swift
+++ UalaCities/CitiesScreenViewModel.swift
@@ -26,22 +26,15 @@ class CitiesScreenViewModel: ObservableObject {
     }
     
     func onAppear() {
-        self.isShowingSpinner = true
-        let request = HTTPRequest(urlString: "https://gist.githubusercontent.com/hernan-uala/dce8843a8edbe0b0018b32e137bc2b3a/raw/0996accf70cb0ca0e16f9a99e0ee185fafca7af1/cities.json")
-        httpClient.send(request) { [weak self] response in
-            guard let self else { return }
-            self.isShowingSpinner = false
-            do {
-                let cities = try JSONDecoder().decode([City].self, from: response.data ?? .init())
-                self.isShowingList = true
-                self.citiesListItems = cities
-            } catch {
-                self.isShowingError = true
-            }
-        }
+        fetchCities()
     }
     
     func onErrorTap() {
+        fetchCities()
+    }
+    
+    private func fetchCities() {
+        self.isShowingList = false
         self.isShowingError = false
         self.isShowingSpinner = true
         let request = HTTPRequest(urlString: "https://gist.githubusercontent.com/hernan-uala/dce8843a8edbe0b0018b32e137bc2b3a/raw/0996accf70cb0ca0e16f9a99e0ee185fafca7af1/cities.json")
@@ -55,8 +48,7 @@ class CitiesScreenViewModel: ObservableObject {
             } catch {
                 self.isShowingError = true
             }
-        }
-    }
+        }    }
 }
 
 struct City: Codable {
